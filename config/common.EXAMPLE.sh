1358be869b7cda64163df5d9149866df6ff1f502
Juan Altmayer Pizzorno <juan@altmayer.com>
2024-04-02 11:37:43 -0400

    - removing common.sh and adding an example file;

---
+++ config/common.EXAMPLE.sh
@@ -0,0 +1,6 @@
+#
+# If you create a file named "common.sh" in this directory, it will be executed by
+# scripts/run_coverup.sh for all configurations
+#
+# You can use it to set your OpenAI or other keys:
+#export OPENAI_API_KEY="sk-..."
--- config/common.sh
+++
@@ -1,4 +0,0 @@
-# This file is executed by scripts/run_coverup.sh for all configurations
-#
-# You can use it to set your OpenAI or other keys:
-#export OPENAI_API_KEY="sk-..."
