3536b92c353643bbd111a9617dfb0d4df03421c2
Juan Altmayer Pizzorno <juan@altmayer.com>
2025-04-09 17:08:54 -0400

    - adding configuration used for RQ5 ablations;

---
+++ config/gpt4o-v2-no-coverage.sh
@@ -0,0 +1,6 @@
+SLIPCOVER_ARGS=""
+COVERUP_ARGS="--model gpt-4o-2024-05-13 --max-attempts 3 --isolate-tests --prompt-family gpt-v2-no-coverage"
+COVERUP_ARGS+=" --install-missing-modules --write-requirements-to missing-modules.txt"
+PYTEST_FINAL_ARGS=" --cleanslate"
+
+COVERUP_ARGS+=" --repeat-tests 5"
